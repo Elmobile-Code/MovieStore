e4d7e154ac05aefe46ab4f448ae93eb7ee2a69fa
my-carpediem <steve.son.1227@gmail.com>
2025-01-31 15:16:16 -0500

    setting up the model to hold the data

--- movies/models.py
+++ movies/models.py
@@ -6,6 +6,6 @@ class Movie(models.Model):
     name = models.CharField(max_length = 255)
     price = models.DecimalField(max_digits = 10, decimal_places = 2, default = 0)
     description = models.TextField()
-    image = models.ImageField(upload_to = 'movie_images/')
+    image_url = models.URLField()
     def __str__(self):
         return str(self.id) + ' ' + self.name
