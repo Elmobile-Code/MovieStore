4159443364f883c5bd9de873d8dd727a190e2ff5
koan <koan329@gmail.com>
2025-02-07 12:23:22 -0500

    added password reset

--- MovieStore/settings.py
+++ MovieStore/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Login',
+    'Accounts',
     'Home',
     'movies',
 ]
@@ -132,3 +132,10 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'in-v3.mailjet.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "973be0097cec82a8dac26f5d0c053ad7"
+EMAIL_HOST_PASSWORD = "2b0d813211841520172a90d8cb7767de"
